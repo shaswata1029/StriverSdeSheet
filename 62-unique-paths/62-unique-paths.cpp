8762788121807e04716f62cf111c6136e4817bc2
Shaswata Raha <shaswata1029@gmail.com>
2022-06-03 13:01:43 +0530

    Time: 3 ms (41.01%), Space: 6.6 MB (26.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
+    int findTotalPaths(int row,int col,int m,int n,vector<vector<int>>&dp){
+        if(row==m-1 and col==n-1)
+            return 1;
+        
+        if(row>=m or col>=n)
+            return 0;
+        
+        if(dp[row][col]!=-1)
+            return dp[row][col];
+        
+        return dp[row][col]=findTotalPaths(row+1,col,m,n,dp)+findTotalPaths(row,col+1,m,n,dp);
+    }
+    
     int uniquePaths(int m, int n) {
         
-        int N=(m+n-2);
-        int rows=(m-1);
+//         using dynamic programming
+//         better than recursive
         
-        double total=1;
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        int total=findTotalPaths(0,0,m,n,dp);
+        return total;
         
-        for(int i=1;i<=rows;i++){
-            total=total*(N-rows+i)/i;
-        }
+//         using combinatorics
+//             most optimal
+//         int N=(m+n-2);
+//         int rows=(m-1);
         
-        return (int)total;
+//         double total=1;
+        
+//         for(int i=1;i<=rows;i++){
+//             total=total*(N-rows+i)/i;
+//         }
+        
+//         return (int)total;
         
     }
 };
