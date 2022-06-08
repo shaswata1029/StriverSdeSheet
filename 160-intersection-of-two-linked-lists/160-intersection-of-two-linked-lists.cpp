7d9f020aca9802378ac7e6d0b8b236e125b38969
Shaswata Raha <shaswata1029@gmail.com>
2022-06-08 20:21:04 +0530

    Time: 88 ms (21.86%), Space: 14.5 MB (72.33%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -11,40 +11,63 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
 //         Approach-1
+//         ListNode *curNode1=headA,*curNode2=headB;
+//         int len1=0,len2=0;
+        
+//         while(curNode1!=NULL)
+//         {
+//             curNode1=curNode1->next;
+//             len1++;
+//         }
+        
+//         while(curNode2!=NULL){
+//             curNode2=curNode2->next;
+//             len2++;
+//         }
+        
+//         curNode1=headA,curNode2=headB;
+//         if(len1>len2){
+//             swap(curNode1,curNode2);
+//         }
+        
+//         int diff=abs(len1-len2);
+//         while(diff>0){
+//             curNode2=curNode2->next;
+//             diff--;
+//         }
+        
+//         while(curNode1!=NULL){
+//             if(curNode1==curNode2)
+//                 return curNode1;
+//             curNode1=curNode1->next;
+//             curNode2=curNode2->next;
+//         }
+        
+        
+//         return NULL;
+        
+        
+//         Approach-2
+        
         ListNode *curNode1=headA,*curNode2=headB;
-        int len1=0,len2=0;
         
-        while(curNode1!=NULL)
-        {
-            curNode1=curNode1->next;
-            len1++;
-        }
-        
-        while(curNode2!=NULL){
-            curNode2=curNode2->next;
-            len2++;
-        }
-        
-        curNode1=headA,curNode2=headB;
-        if(len1>len2){
-            swap(curNode1,curNode2);
-        }
-        
-        int diff=abs(len1-len2);
-        while(diff>0){
-            curNode2=curNode2->next;
-            diff--;
-        }
-        
-        while(curNode1!=NULL){
+        while(curNode1!=NULL or curNode2!=NULL){
+            
+            if(curNode1==NULL)
+               curNode1=headB;
+               
+               if(curNode2==NULL)
+               curNode2=headA;
+            
             if(curNode1==curNode2)
                 return curNode1;
+            
             curNode1=curNode1->next;
             curNode2=curNode2->next;
         }
         
-        
         return NULL;
+        
         
         
     }
