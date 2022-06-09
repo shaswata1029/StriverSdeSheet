5e2c10204f3d2bc7e7b959315196c7b4c13a6685
Shaswata Raha <shaswata1029@gmail.com>
2022-06-09 10:25:24 +0530

    Time: 20 ms (63.36%), Space: 11.5 MB (72.42%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -10,13 +10,33 @@
  */
 class Solution {
 public:
+//     Recursive function
+    ListNode* reverseK(ListNode *head,int curLen,int k){
+        if(curLen<k)
+            return head;
+        
+        ListNode*prevNode=NULL,*nextNode=NULL,*curNode=head;
+        
+        for(int i=0;i<k;i++){
+            nextNode=curNode->next;
+            curNode->next=prevNode;
+            prevNode=curNode;
+            curNode=nextNode;
+        }
+        
+        
+        head->next=reverseK(curNode,curLen-k,k);
+        return prevNode;
+        
+        
+        
+    }
+    
     ListNode* reverseKGroup(ListNode* head, int k) {
         
         if(head==NULL || k==1)
             return head;
         
-        
-//         Iterative approach
         ListNode *curNode=head;
         int len=0;
         
@@ -25,35 +45,39 @@ public:
             curNode=curNode->next;
         }
         
-        ListNode *dummyNode=new ListNode(-1);
-        dummyNode->next=head;
-        ListNode* prevHead=dummyNode;
-        curNode=head;
+//         Iterative approach
+//         ListNode *dummyNode=new ListNode(-1);
+//         dummyNode->next=head;
+//         ListNode* prevHead=dummyNode;
+//         curNode=head;
         
-        while(len>=k){
+//         while(len>=k){
             
-            int tempLen=k;
-            ListNode* prevNode=NULL;
-            ListNode *nextNode=NULL;
-            ListNode *tempNode=curNode;
+//             int tempLen=k;
+//             ListNode* prevNode=NULL;
+//             ListNode *nextNode=NULL;
+//             ListNode *tempNode=curNode;
             
-            while(tempLen>0){
-                nextNode=curNode->next;
-                curNode->next=prevNode;
-                prevNode=curNode;
-                curNode=nextNode;
-                tempLen--;
-            }
+//             while(tempLen>0){
+//                 nextNode=curNode->next;
+//                 curNode->next=prevNode;
+//                 prevNode=curNode;
+//                 curNode=nextNode;
+//                 tempLen--;
+//             }
             
-            prevHead->next=prevNode;
-            prevHead=tempNode;
+//             prevHead->next=prevNode;
+//             prevHead=tempNode;
             
-            len-=k;
-        }
+//             len-=k;
+//         }
         
-        prevHead->next=curNode;
+//         prevHead->next=curNode;
         
-        return dummyNode->next;
+//         return dummyNode->next;
+        
+//         Recursive approach
+        return reverseK(head,len,k);
         
     }
 };
