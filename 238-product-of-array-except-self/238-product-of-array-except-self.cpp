a0203e4614e4bcaf91b8a8f7a7d1e2ce251aa059
Shaswata Raha <shaswata1029@gmail.com>
2022-06-12 01:33:00 +0530

    Time: 37 ms (42.93%), Space: 23.8 MB (99.37%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -3,20 +3,32 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int>product(n);
+        vector<int>product(n,1);
         
-        vector<int>leftProduct(n,1);
-        vector<int>rightProduct(n,1);
+//      Using Extra Space   
+//         vector<int>leftProduct(n,1);
+//         vector<int>rightProduct(n,1);
+        
+//         for(int i=1;i<n;i++)
+//             leftProduct[i]=leftProduct[i-1]*nums[i-1];
+        
+        
+//         for(int i=n-2;i>=0;i--)
+//             rightProduct[i]=rightProduct[i+1]*nums[i+1];
+        
+//         for(int i=0;i<n;i++)
+//             product[i]=leftProduct[i]*rightProduct[i];
+        
+//         Without using extra space
         
         for(int i=1;i<n;i++)
-            leftProduct[i]=leftProduct[i-1]*nums[i-1];
+            product[i]=product[i-1]*nums[i-1];
         
-        
-        for(int i=n-2;i>=0;i--)
-            rightProduct[i]=rightProduct[i+1]*nums[i+1];
-        
-        for(int i=0;i<n;i++)
-            product[i]=leftProduct[i]*rightProduct[i];
+        int rightProduct=1;
+        for(int i=n-2;i>=0;i--){
+            rightProduct=rightProduct*nums[i+1];
+            product[i]=product[i]*rightProduct;
+        }
         
         return product;
         
