05eaa10e57e7ad6b7d7dc781f4b406824cf1dee0
Shaswata Raha <shaswata1029@gmail.com>
2022-06-17 16:35:38 +0530

    Time: 3 ms (93.08%), Space: 13.1 MB (66.62%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+//     static bool compare(const pair<int,int>&p1,const pair<int,int>&p2){
+//         if(p1.first!=p2.first)
+//             return p1.first<p2.first;
+        
+//         return p1.second<p2.second
+//     }
+    
+    void verticalTraversal(TreeNode* root,int level,int column,map<int,vector<pair<int,int>>>&hashMap){
+        if(root==NULL)
+            return;
+        
+        hashMap[column].push_back({level,root->val});
+        verticalTraversal(root->left,level+1,column-1,hashMap);
+        verticalTraversal(root->right,level+1,column+1,hashMap);
+    }
+    
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,vector<pair<int,int>>>hashMap;
+        
+        verticalTraversal(root,0,0,hashMap);
+        
+        vector<vector<int>>verticalOrder;
+        for(auto &ele:hashMap){
+            auto &v=ele.second;
+            sort(v.begin(),v.end());
+            
+            vector<int>temp;
+            for(auto val:v){
+                temp.push_back(val.second);
+            }
+            
+            verticalOrder.push_back(temp);
+        }
+        
+        return verticalOrder;
+    }
+};
