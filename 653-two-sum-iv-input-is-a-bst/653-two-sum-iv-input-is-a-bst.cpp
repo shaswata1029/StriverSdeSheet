054472d2958f198f6e6f3ec72e8131ee062fb928
Shaswata Raha <shaswata1029@gmail.com>
2022-06-23 12:05:09 +0530

    Time: 66 ms (41.60%), Space: 36.6 MB (91.72%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,91 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator{
+    stack<TreeNode*>nextStack;
+    stack<TreeNode*>prevStack;
+    
+    public:
+    BSTIterator(TreeNode *root){
+        TreeNode *cur=root;
+        
+        while(cur!=NULL){
+            nextStack.push(cur);
+            cur=cur->left;
+        }
+        
+        cur=root;
+        while(cur!=NULL){
+            prevStack.push(cur);
+            cur=cur->right;
+        }
+    }
+    
+    int next(){
+        if(nextStack.empty())
+            return -1;
+        
+        TreeNode *cur=nextStack.top();
+        nextStack.pop();
+        int val=cur->val;
+        
+        cur=cur->right;
+        
+        while(cur!=NULL){
+            nextStack.push(cur);
+            cur=cur->left;
+        }
+        
+        return val;
+    }
+    
+    int before(){
+        if(prevStack.empty())
+            return -1;
+        
+        TreeNode *cur=prevStack.top();
+        prevStack.pop();
+        int val=cur->val;
+        
+        cur=cur->left;
+        
+        while(cur!=NULL){
+            prevStack.push(cur);
+            cur=cur->right;
+        }
+        
+        return val;
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int target) {
+        
+        BSTIterator it(root);
+        
+        int left=it.next();
+        int right=it.before();
+        
+        while(left<right){
+            int sum=left+right;
+            if(sum==target)
+                return true;
+            else if(sum>target)
+                right=it.before();
+            else
+                left=it.next();
+        }
+        
+        return false;
+        
+    }
+};
