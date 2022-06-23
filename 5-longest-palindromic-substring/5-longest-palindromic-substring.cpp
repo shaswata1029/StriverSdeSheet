50575208109a5e830c8c36c94382c65bcc406c18
Shaswata Raha <shaswata1029@gmail.com>
2022-06-23 22:19:48 +0530

    Time: 32 ms (80.04%), Space: 7 MB (97.82%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
+    int findLongestSubstring(string &str,int n,int start,int end){
+        
+        while(start>=0 && end<n && str[start]==str[end]){
+            start--;
+            end++;
+        }
+        
+        return (end-start-1);
+    }
+    
     string longestPalindrome(string s) {
         
         int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,false));
-        int start=0,end=0,maxLen=1;
+        int start=0,maxLen=1;
         
-        for(int gap=0;gap<n;gap++){
-            int i=0;
-            int j=gap;
+        for(int k=0;k<n;k++){
+            int len=findLongestSubstring(s,n,k,k);
+            len=max(len,findLongestSubstring(s,n,k,k+1));
             
-            while(i<n && j<n){
-                if(gap==0)
-                    dp[i][j]=true;
-                else if(gap==1)
-                    dp[i][j]=(s[i]==s[j]);
-                else
-                    dp[i][j]=(s[i]==s[j] && (dp[i+1][j-1]));
-                
-                if(dp[i][j] && gap+1>maxLen){
-                    maxLen=gap+1;
-                    start=i;
-                    end=j;
-                }
-                
-                i++;
-                j++;
+            if(len>maxLen){
+                maxLen=len;
+                start=k-(len-1)/2;
             }
         }
+        
+         
         
         
         string res=s.substr(start,maxLen);
