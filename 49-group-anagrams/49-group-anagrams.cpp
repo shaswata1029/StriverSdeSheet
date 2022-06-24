fda5c0abaf98b9b76ae80a3ee74ca4dc79e4c832
Shaswata Raha <shaswata1029@gmail.com>
2022-06-24 20:32:30 +0530

    Time: 175 ms (7.18%), Space: 37.6 MB (5.01%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,12 +3,23 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
 //         Using Sorting
-        unordered_map<string,vector<string>>hashMap;
+//         unordered_map<string,vector<string>>hashMap;
+        
+//         for(auto &str:strs){
+//             string temp=str;
+//             sort(temp.begin(),temp.end());
+//             hashMap[temp].push_back(str);
+//         }
+        
+//           Using Map of Map
+        map<map<char,int>,vector<string>>hashMap;
         
         for(auto &str:strs){
-            string temp=str;
-            sort(temp.begin(),temp.end());
-            hashMap[temp].push_back(str);
+            map<char,int>freq;
+            
+            for(auto ch:str)
+                freq[ch]++;
+            hashMap[freq].push_back(str);
         }
         
         vector<vector<string>>res;
@@ -18,5 +29,7 @@ public:
         }
         
         return res;
+    
+        
     }
 };
