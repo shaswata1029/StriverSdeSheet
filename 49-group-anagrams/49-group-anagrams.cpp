adb82e5410fea3515f240a91ed302d60d89ee269
Shaswata Raha <shaswata1029@gmail.com>
2022-06-24 20:32:56 +0530

    Time: 67 ms (34.72%), Space: 19.6 MB (80.53%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,24 +3,24 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
 //         Using Sorting
-//         unordered_map<string,vector<string>>hashMap;
-        
-//         for(auto &str:strs){
-//             string temp=str;
-//             sort(temp.begin(),temp.end());
-//             hashMap[temp].push_back(str);
-//         }
-        
-//           Using Map of Map
-        map<map<char,int>,vector<string>>hashMap;
+        unordered_map<string,vector<string>>hashMap;
         
         for(auto &str:strs){
-            map<char,int>freq;
-            
-            for(auto ch:str)
-                freq[ch]++;
-            hashMap[freq].push_back(str);
+            string temp=str;
+            sort(temp.begin(),temp.end());
+            hashMap[temp].push_back(str);
         }
+        
+//           Using Map of Map
+//         map<map<char,int>,vector<string>>hashMap;
+        
+//         for(auto &str:strs){
+//             map<char,int>freq;
+            
+//             for(auto ch:str)
+//                 freq[ch]++;
+//             hashMap[freq].push_back(str);
+//         }
         
         vector<vector<string>>res;
         
