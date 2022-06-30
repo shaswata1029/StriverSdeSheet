e24df76a633267cb7e0f8f214b0b493acbd8161a
Shaswata Raha <shaswata1029@gmail.com>
2022-06-30 20:43:01 +0530

    Time: 24 ms (85.82%), Space: 16.3 MB (92.36%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -13,8 +13,10 @@ public:
             st.push(val*1LL);
             minVal=val;
         }else if(val>=minVal)
+//             If val is greater than the minimum no need to update the minimum
             st.push(val*1LL);
         else{
+//             Update the minimum to val and push into the stack the modified val such that it is always lesser than the minimum
             long long newVal=2LL*val-minVal;
             st.push(newVal);
             minVal=val;
@@ -23,6 +25,7 @@ public:
     }
     
     void pop() {
+//         If the top is lesser than the minimum then we need to revert back to the previous minimum after popping from the stack using the formula 
         if(st.top()<minVal)
         {
             long long val=st.top();
@@ -33,6 +36,7 @@ public:
     }
     
     int top() {
+//         If the top is lesser than the minimum then the top has modified val and the top element is the min value only
         if(st.top()<minVal)
             return minVal;
         
