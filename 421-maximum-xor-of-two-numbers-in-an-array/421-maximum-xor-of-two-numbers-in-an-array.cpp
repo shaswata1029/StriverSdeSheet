09dbffb00eb46a635d3577afb4762a1a61a6b934
Shaswata Raha <shaswata1029@gmail.com>
2022-07-22 21:37:02 +0530

    Time: 1164 ms (10.76%), Space: 142.2 MB (11.22%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,89 @@
+class Node{
+    vector<Node*>child;
+    
+    public:
+    Node(){
+        for(int index=0;index<2;index++)
+            child.push_back(NULL);
+    }
+    
+    bool containsKey(int bit){
+        if(child[bit]==NULL)
+            return false;
+        return true;
+    }
+    
+    void put(int bit){
+        child[bit]=new Node();
+    }
+    
+    Node* getNext(int bit){
+        return child[bit];
+    }
+};
+
+class Trie{
+  Node* root;
+    public:
+    Trie(){
+        root=new Node();
+    }
+    
+    void insert(int num){
+        Node*node=root;
+        
+        for(int index=31;index>=0;index--){
+            int bit=(num>>index) & 1;
+            if(!node->containsKey(bit)){
+                node->put(bit);
+            }
+            
+            node=node->getNext(bit);
+        }
+    }
+    
+    int getMaxXor(int num){
+        Node*node=root;
+        
+        int maxNum=0;
+        for(int index=31;index>=0;index--){
+            int bit=(num>>index) & 1;
+            if(node->containsKey(!bit)){
+                maxNum= maxNum |(1<<index);
+                node=node->getNext(!bit);
+            }else{
+                node=node->getNext(bit);
+            }
+        }
+        
+        return maxNum;
+    }
+    
+};
+
+class Solution {
+public:
+    
+    
+    int findMaximumXOR(vector<int>& nums) {
+        
+        Trie trie;
+        
+        int size=nums.size();
+        
+        for(int index=0;index<size;index++){
+            int num=nums[index];
+            trie.insert(num);
+        }
+        
+        int maxXor=0;
+        
+        for(int index=0;index<size;index++){
+            int curXor=trie.getMaxXor(nums[index]);
+            maxXor=max(maxXor,curXor);
+        }
+        
+        return maxXor;
+        
+    }
+};
