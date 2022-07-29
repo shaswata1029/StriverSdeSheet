60671ef949a4186d65b2c250273e020dcf8e49d9
Shaswata Raha <shaswata1029@gmail.com>
2022-07-29 10:51:04 +0530

    Time: 12 ms (69.57%), Space: 9.7 MB (47.05%) - LeetHub

--- 685-redundant-connection-ii/685-redundant-connection-ii.cpp
+++ 685-redundant-connection-ii/685-redundant-connection-ii.cpp
@@ -41,6 +41,9 @@ int find(vector<vector<int>>&edges,int n,int edgeIndex){
     
     DSU d(n);
     
+    // returning the edge index which causes a cycle  in case there is a cycle otherwise returning -1 signifying no cycle is present
+//     This method of indentfying cycle in a directed graph can only be applied if we are sure there is a cycle in the graph and there is only one cycle since dsu applies mailny to undirected graphs
+    
     for(int index=0;index<n;index++){
         if(index!=edgeIndex){
         auto &edge=edges[index];
@@ -77,20 +80,26 @@ public:
             x--;
             y--;
             
+//             Setting the index of edge which is the first parent
             if(indegree[y]==-1)
                 indegree[y]=index;
             else{
+//                 If one node has two parents then there may or may not be a cylcle
                 int ans=find(edges,n,index);
+//                 check that using removing the current index if there is a cycle
+//                 If a cycle is present even after removing the edge then the previous edge of the node is the answer
                 if(ans!=-1)
                     removedEdge=indegree[y];
                 else
+//                If a cycle is not  present then this edge will be the answer
                     removedEdge=index;
                 break;
             }
         }
         
     
-        
+//         One last case which may arise is every node has a parent including a root node which causes a cycle 
+//         In that case we simply remove the last node which is causing a cycle by checking it with DSU
         if(removedEdge==-1){
             removedEdge=find(edges,n,-1);
         }
