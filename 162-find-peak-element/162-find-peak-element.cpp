ed677c94e17fed62161ac25458564a95fd0d3463
Shaswata Raha <shaswata1029@gmail.com>
2022-08-09 10:35:12 +0530

    Time: 6 ms (59.40%), Space: 8.8 MB (89.58%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -7,25 +7,18 @@ public:
         if(n==1)
             return 0;
         
-        int low=0;
-        int high=n-1;
+        if(arr[0]>arr[1])
+            return 0;
+        
+        if(arr[n-1]>arr[n-2])
+            return n-1;
+        
+        int low=1;
+        int high=n-2;
         
         while(low<=high){
             int mid=low+(high-low)/2;
             
-            if(mid==0 && arr[mid]>arr[mid+1])
-                return mid;
-            else if(mid==0){
-                low=mid+1;
-                continue;
-            }
-            
-            if(mid==n-1 && arr[mid]>arr[mid-1])
-                return mid;
-            else if(mid==n-1){
-                high=mid-1;
-                continue;
-            }
             
             int cur_val=arr[mid];
             int prev_val=arr[mid-1];
