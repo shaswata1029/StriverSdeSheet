49b7df08ff19d643a441989166aab442b4c22c67
Shaswata Raha <shaswata1029@gmail.com>
2022-08-19 10:35:58 +0530

    Time: 203 ms (30.22%), Space: 58.5 MB (27.74%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        
+      map<int,int>m;
+        
+        for(auto ele:nums)
+            m[ele]++;
+        
+        for(auto ele:m){
+            int val=ele.first;
+            int freq=ele.second;
+        
+            
+            int f1=0,f2=0,f3=0,f4=0;
+            
+            if(m.find(val-1)!=m.end())
+                f1=m[val-1];
+            
+            if(m.find(val-2)!=m.end())
+                f2=m[val-2];
+            
+            if(m.find(val+1)!=m.end())
+                f3=m[val+1];
+            
+            if(m.find(val+2)!=m.end())
+                f4=m[val+2];
+            
+//             acting as last
+            int freq1=min(f1,f2);
+//             acting as first
+            int freq2=min(f3,f4);
+            
+//             acting as middle
+            f1-=freq1;
+            f3-=freq2;
+            
+            int freq3=min(f1,f3);
+            if(freq1+freq2+freq3<freq)
+                return false;
+        }
+        
+        return true;
+    }
+};
